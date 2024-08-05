a377889593a70a538c1bc3181f8bdc529481d9cf
elena.valik <alenka.valik@yandex.ru>
2024-08-05 18:38:47 +0500

    Recording stream with decoding frames and fixed bugs

--- server/src/ffmpeg.js
+++ server/src/ffmpeg.js
@@ -3,6 +3,8 @@ const { EventEmitter } = require('events');
 const { createSdpText } = require('./sdp');
 const { convertStringToStream } = require('./utils');
 const fs = require('fs');
+const { Transform } = require('stream');
+const sharp = require('sharp');
 
 const RECORD_FILE_LOCATION_PATH = process.env.RECORD_FILE_LOCATION_PATH || './files';
 
@@ -10,7 +12,6 @@ module.exports = class FFmpeg {
   constructor(rtpParameters) {
     this._rtpParameters = rtpParameters;
     this._videoProcess = undefined;
-    this._frameProcess = undefined;
     this._observer = new EventEmitter();
     this._firstFrameCaptured = false;
     this._lastFrameCaptured = false;
@@ -21,41 +22,76 @@ module.exports = class FFmpeg {
 
   async _start() {
     try {
-      await this._captureFirstFrame();
-      await this._startVideoRecording();
+      await this._startVideoRecordingAndFrameDecoding();
       await this._waitForProcessClose();
-      await this._captureLastFrame();
     } catch (error) {
       console.error('Error during FFmpeg process:', error);
     }
   }
 
-  async _startVideoRecording() {
-    console.log('Starting video recording');
+  async _startVideoRecordingAndFrameDecoding() {
+    console.log('Starting video recording and frame decoding');
     console.log('FFmpeg arguments:', this._commandArgs);
 
     return new Promise((resolve, reject) => {
       this._videoProcess = child_process.spawn('ffmpeg', this._commandArgs);
 
       let stderr = '';
-      if (this._videoProcess.stderr) {
-        this._videoProcess.stderr.setEncoding('utf-8');
-        this._videoProcess.stderr.on('data', data => {
-          stderr += data;
-          console.log('ffmpeg::process::data [data:%o]', data);
-        });
+      this._videoProcess.stderr.setEncoding('utf-8');
+      this._videoProcess.stderr.on('data', data => {
+        stderr += data;
+        console.log('ffmpeg::process::data [data:%o]', data);
+      });
+
+      const width = 640;
+      const height = 480;
+      const frameSize = width * height * 3;
+
+      class FrameProcessor extends Transform {
+        constructor(rtpParameters, options) {
+          super(options);
+          this.remainingBuffer = Buffer.alloc(0);
+          this.rtpParameters = rtpParameters;
+          this.firstFrameCaptured = false;
+        }
+
+        async _transform(chunk, encoding, callback) {
+          this.remainingBuffer = Buffer.concat([this.remainingBuffer, chunk]);
+
+          while (this.remainingBuffer.length >= frameSize) {
+            const frameBuffer = this.remainingBuffer.slice(0, frameSize);
+            this.remainingBuffer = this.remainingBuffer.slice(frameSize);
+
+            try {
+
+              if (!this.firstFrameCaptured) {
+                await sharp(frameBuffer, { raw: { width, height, channels: 3 } })
+                    .jpeg()
+                    .toFile(`${RECORD_FILE_LOCATION_PATH}/start_frame_${this.rtpParameters.fileName}.jpg`);
+                this.firstFrameCaptured = true;
+              }
+
+              await sharp(frameBuffer, { raw: { width, height, channels: 3 } })
+                  .jpeg()
+                  .toFile(`${RECORD_FILE_LOCATION_PATH}/end_frame_${this.rtpParameters.fileName}.jpg`);
+            } catch (err) {
+              console.error('Error processing frame:', err);
+            }
+          }
+
+          callback();
+        }
       }
 
-      if (this._videoProcess.stdout) {
-        this._videoProcess.stdout.setEncoding('utf-8');
-        this._videoProcess.stdout.on('data', data => console.log('ffmpeg::process::data [data:%o]', data));
-      }
+      const frameProcessor = new FrameProcessor(this._rtpParameters);
 
-      this._videoProcess.on('message', message => console.log('ffmpeg::process::message [message:%o]', message));
+      this._videoProcess.stdout.pipe(frameProcessor);
+
       this._videoProcess.on('error', error => {
         console.error('ffmpeg::process::error [error:%o]', error);
         reject(error);
       });
+
       this._videoProcess.once('close', (code) => {
         console.log('ffmpeg::process::close [code:%o]', code);
         if (code === 0) {
@@ -67,122 +103,39 @@ module.exports = class FFmpeg {
         }
       });
 
-      this._sdpStream.on('error', error => console.error('sdpStream::error [error:%o]', error));
-
-      this._sdpStream.resume();
       this._sdpStream.pipe(this._videoProcess.stdin);
     });
   }
 
-  async _waitForProcessClose() {
+  _waitForProcessClose() {
     return new Promise((resolve) => {
       this._observer.once('process-close', resolve);
     });
   }
 
-  async _captureFrame(time, outputFileName) {
-    console.log('captureFrame() [time:%s, outputFileName:%s]', time, outputFileName);
-
-    const outputPath = `${RECORD_FILE_LOCATION_PATH}/${outputFileName}`;
-    console.log('Saving frame to:', outputPath);
-
-    return new Promise((resolve, reject) => {
-      const args = [
-        '-loglevel', 'debug',
-        '-protocol_whitelist', 'pipe,udp,rtp',
-        '-fflags', '+genpts',
-        '-f', 'sdp',
-        '-i', 'pipe:0',
-        '-ss', time,
-        '-frames:v', '1',
-        '-q:v', '2',
-        outputPath
-      ];
-
-      this._frameProcess = child_process.spawn('ffmpeg', args);
-
-      let stderr = '';
-      this._frameProcess.stderr.setEncoding('utf-8');
-      this._frameProcess.stderr.on('data', data => {
-        stderr += data;
-        console.log('ffmpeg::captureFrame::data [data:%o]', data);
-      });
-
-      this._frameProcess.stdout.setEncoding('utf-8');
-      this._frameProcess.stdout.on('data', data => console.log('ffmpeg::captureFrame::data [data:%o]', data));
-
-      this._frameProcess.on('error', error => {
-        console.error('ffmpeg::captureFrame::error [error:%o]', error);
-        reject(error);
-      });
-
-      this._frameProcess.once('close', (code) => {
-        console.log('ffmpeg::captureFrame::close [code:%o]', code);
-        if (code === 0) {
-          resolve();
-        } else {
-          console.error('FFmpeg captureFrame stderr output:', stderr);
-          reject(new Error(`FFmpeg process exited with code ${code}`));
-        }
-      });
-
-      const sdpStreamCopy = convertStringToStream(this._sdpString);
-      sdpStreamCopy.pipe(this._frameProcess.stdin);
-      sdpStreamCopy.on('error', error => console.error('sdpStream::error [error:%o]', error));
-    });
-  }
-
-  async _captureFirstFrame() {
-    await this._captureFrame('00:00:00', `start_frame_${this._rtpParameters.fileName}.jpg`)
-        .then(() => {
-          this._firstFrameCaptured = true;
-          this._checkIfBothFramesCaptured();
-        })
-        .catch(err => console.error('Error capturing first frame:', err));
-  }
-
-  async _captureLastFrame() {
-    await this._captureFrame('00:00:00', `end_frame_${this._rtpParameters.fileName}.jpg`)
-        .then(() => {
-          this._lastFrameCaptured = true;
-          this._checkIfBothFramesCaptured();
-        })
-        .catch(err => console.error('Error capturing last frame:', err));
-  }
-
-  _checkIfBothFramesCaptured() {
-    if (this._firstFrameCaptured && this._lastFrameCaptured) {
-      console.log('Both first and last frames captured.');
-    }
-  }
-
   kill() {
-    console.log('kill() [pid:%d]', this._videoProcess.pid);
-    this._videoProcess.kill('SIGINT');
-    if (this._frameProcess) {
-      console.log('kill() frame process [pid:%d]', this._frameProcess.pid);
-      this._frameProcess.kill('SIGINT');
-    }
+    console.log('kill() [pid:%d]', this._videoProcess?.pid);
+    if (this._videoProcess) this._videoProcess.kill('SIGINT');
     this._observer.emit('process-close');
   }
 
-  get _commandArgs () {
+  get _commandArgs() {
+    const uniquePort = 5004;
     return [
-      '-loglevel',
-      'debug',
-      '-protocol_whitelist',
-      'pipe,udp,rtp',
-      '-fflags',
-      '+genpts',
-      '-f',
-      'sdp',
-      '-i',
-      'pipe:0',
-      '-map',
-      '0:v:0',
-      '-c:v',
-      'copy',
-      `${RECORD_FILE_LOCATION_PATH}/${this._rtpParameters.fileName}.webm`
+      '-loglevel', 'debug',
+      '-protocol_whitelist', 'pipe,udp,rtp',
+      '-fflags', '+genpts',
+      '-f', 'sdp',
+      '-i', 'pipe:0',
+      '-map', '0:v:0',
+      '-c:v', 'copy',
+      `${RECORD_FILE_LOCATION_PATH}/${this._rtpParameters.fileName}.webm`,
+      '-vf', 'fps=1',
+      '-f', 'image2pipe',
+      '-vcodec', 'rawvideo',
+      '-pix_fmt', 'rgb24',
+      'pipe:1',
+      '-rtpflags', `localport=${uniquePort}`
     ];
   }
-}
+};
--- server/src/sdp.js
+++ server/src/sdp.js
@@ -1,25 +1,39 @@
 const { getCodecInfoFromRtpParameters } = require('./utils');
 
 // File to create SDP text from mediasoup RTP Parameters
-module.exports.createSdpText = (rtpParameters) => {
+module.exports.createSdpText = (rtpParameters, videoPortOffset = 0, audioPortOffset = 0) => {
   const { video, audio } = rtpParameters;
-  
+
+  if (!video || !audio) {
+    throw new Error('Missing video or audio parameters in RTP parameters.');
+  }
+
   // Video codec info
   const videoCodecInfo = getCodecInfoFromRtpParameters('video', video.rtpParameters);
+  if (!videoCodecInfo) {
+    throw new Error('Invalid video codec info.');
+  }
 
   // Audio codec info
   const audioCodecInfo = getCodecInfoFromRtpParameters('audio', audio.rtpParameters);
+  if (!audioCodecInfo) {
+    throw new Error('Invalid audio codec info.');
+  }
+
+  // Adjust ports for multiple processes
+  const videoPort = video.remoteRtpPort + videoPortOffset;
+  const audioPort = audio.remoteRtpPort + audioPortOffset;
 
   return `v=0
-  o=- 0 0 IN IP4 127.0.0.1
-  s=FFmpeg
-  c=IN IP4 127.0.0.1
-  t=0 0
-  m=video ${video.remoteRtpPort} RTP/AVP ${videoCodecInfo.payloadType} 
-  a=rtpmap:${videoCodecInfo.payloadType} ${videoCodecInfo.codecName}/${videoCodecInfo.clockRate}
-  a=sendonly
-  m=audio ${audio.remoteRtpPort} RTP/AVP ${audioCodecInfo.payloadType} 
-  a=rtpmap:${audioCodecInfo.payloadType} ${audioCodecInfo.codecName}/${audioCodecInfo.clockRate}/${audioCodecInfo.channels}
-  a=sendonly
-  `;
+o=- 0 0 IN IP4 127.0.0.1
+s=FFmpeg
+c=IN IP4 127.0.0.1
+t=0 0
+m=video ${videoPort} RTP/AVP ${videoCodecInfo.payloadType}
+a=rtpmap:${videoCodecInfo.payloadType} ${videoCodecInfo.codecName}/${videoCodecInfo.clockRate}
+a=sendonly
+m=audio ${audioPort} RTP/AVP ${audioCodecInfo.payloadType}
+a=rtpmap:${audioCodecInfo.payloadType} ${audioCodecInfo.codecName}/${audioCodecInfo.clockRate}/${audioCodecInfo.channels}
+a=sendonly
+`;
 };
